fd031acf7b2228302f659448b499d1b4934ea40a
Gouhar Lali <gouhar.lali@hotmail.com>
2024-05-10 10:55:00 +0100

    fix: listItem updated

--- app/src/test/java/com/example/codingassessment/MainAdapterTest.kt
+++ app/src/test/java/com/example/codingassessment/MainAdapterTest.kt
@@ -9,25 +9,6 @@ class MainAdapterTest {
     private val testItems = listOf(
         ListItem("Item 1", System.currentTimeMillis()),
         ListItem("Item 2", System.currentTimeMillis()),
-        ListItem("Item 1", System.currentTimeMillis()),
-        ListItem("Item 2", System.currentTimeMillis()),
-        ListItem("Item 1", System.currentTimeMillis()),
-        ListItem("Item 2", System.currentTimeMillis()),
-        ListItem("Item 1", System.currentTimeMillis()),
-        ListItem("Item 2", System.currentTimeMillis()),
-        ListItem("Item 1", System.currentTimeMillis()),
-        ListItem("Item 2", System.currentTimeMillis()),
-        ListItem("Item 1", System.currentTimeMillis()),
-        ListItem("Item 2", System.currentTimeMillis()),
-        ListItem("Item 1", System.currentTimeMillis()),
-        ListItem("Item 2", System.currentTimeMillis()),
-        ListItem("Item 1", System.currentTimeMillis()),
-        ListItem("Item 2", System.currentTimeMillis()),
-        ListItem("Item 1", System.currentTimeMillis()),
-        ListItem("Item 2", System.currentTimeMillis()),
-        ListItem("Item 1", System.currentTimeMillis()),
-        ListItem("Item 2", System.currentTimeMillis()),
-
     )
 
     private lateinit var adapter: MainAdapter
