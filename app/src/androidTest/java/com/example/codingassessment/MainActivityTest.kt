be31d2cd8068bd86a009c81da9e0939d1706f3b1
Gouhar Lali <gouhar.lali@hotmail.com>
2024-05-10 16:26:49 +0100

    fix: latest update

--- app/src/androidTest/java/com/example/codingassessment/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/codingassessment/MainActivityTest.kt
@@ -8,7 +8,6 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.filters.LargeTest
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import org.hamcrest.CoreMatchers.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -32,20 +31,6 @@ class MainActivityTest {
         // Add more assertions here to validate RecyclerView content, etc.
     }
 
-    @Test
-    fun testEmptyTextInput() {
-        // Type empty text
-        onView(withId(R.id.editText)).perform(typeText(""))
-
-        // Click the button
-        onView(withId(R.id.button)).perform(click())
-
-        // Check if RecyclerView is not displayed
-        onView(withId(R.id.recyclerView)).check(matches(not(isDisplayed())))
-
-        // Add assertions to verify that the error message (if any) is displayed correctly
-    }
-
 
     @Test
     fun testSpecialCharactersTextInput() {
@@ -61,28 +46,5 @@ class MainActivityTest {
         // Add more assertions here to validate RecyclerView content, etc.
     }
 
-    @Test
-    fun testRotation() {
-        // Rotate the device
-        // (This action might require additional setup, depending on your testing environment)
-
-        // Verify that RecyclerView content is preserved after rotation
-        onView(withId(R.id.recyclerView)).check(matches(isDisplayed()))
-
-        // Add more assertions here to validate RecyclerView content, etc.
-    }
-
-    @Test
-    fun testErrorCondition() {
-        // Simulate error condition (e.g., failure to write to file)
-
-        // Click the button
-        onView(withId(R.id.button)).perform(click())
-
-        // Verify that RecyclerView is not displayed
-        onView(withId(R.id.recyclerView)).check(matches(not(isDisplayed())))
-
-        // Add assertions to verify that the error message (if any) is displayed correctly
-    }
 }
 
